ada46d6a8df9b8511f56d7a6b5dc91a9e1cf3574
OmerAtayilmaz <omeratayilmaz@gmail.com>
2022-06-14 18:57:06 +0300

    array and object basics

---
+++ main.js
@@ -0,0 +1,21 @@
+/*->JS: const,var,let 
+  ->JS bir değişkene bütün türler atanabilir!
+  
+//ARRAY
+var apple = ["ahmet", 1, "ahmet@gmail.com", true, 5.4];
+//pop-push-shift-unshift
+console.log(typeof apple); //array
+console.log(apple);
+*/
+
+//OBJECT
+const user = {
+  name: undefined,
+  email: undefined,
+  password: undefined,
+  gender: undefined,
+};
+user.name = "Test";
+
+console.log(user);
+console.log(user.name);
